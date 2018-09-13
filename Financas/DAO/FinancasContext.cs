c6f7eb7dcda62be73baa4f95923bd079687a2061
Danielle Siqueira <danielle-siqueira@outlook.com>
2018-09-13 20:38:54 -0300

    Controle Financeiro em ASP NET MV5 e Razor Pages

---
+++ Financas/DAO/FinancasContext.cs
@@ -0,0 +1,22 @@
+﻿using Financas.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Financas.DAO
+{
+    public class FinancasContext : DbContext
+    {
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Movimentacao> Movimentacoes { get; set; }
+
+        //OnModelCreating podemos alterar o modelagem feito por Entity Framework
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Mapeando chave estrangeira - Onde toda movimentação tem um usuário.
+            modelBuilder.Entity<Movimentacao>().HasRequired(m => m.Usuario);
+        }
+    }
+}
